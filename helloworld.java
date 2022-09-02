9cc36a4e5b24fe0bb72effb506061e2e31d43222
babu <ec2-user@ip-172-31-15-208.ap-south-1.compute.internal>
2022-09-02 17:46:58 +0000

    hello world file added

--- helloworld.java
+++ helloworld.java
@@ -4,7 +4,7 @@ class helloworld
     // Prints "Hello, World" to the terminal window. 
     public static void main(String args[]) 
     { 
-        System.out.println("Hello World and welcome to java"); 
+        System.out.println("Hello new  World and welcome to java"); 
     } 
 }
 
